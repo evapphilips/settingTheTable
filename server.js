579d479b37aefc5439e4babea5fb18d0a676f7c4
evapphilips <ep1521@nyu.edu>
2020-02-28 14:11:04 -0500

    adding mongodb and setup intial server

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-// Require libraries
-var express = require('express');
-var socket = require('socket.io');
+// Require libraries and files
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+const mongoose = require('mongoose')
+
+const usersRouter = require('./routes/users.js')
+
+
+// Connect to the database
+//mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+app.use(express.json())
+
+
+// Setup Routing
+app.use(express.static('public'));
+app.use('/user', usersRouter)
 
 
 // App setup
-var app = express();
-var server = app.listen(8080, function () {
+const server = app.listen(8080, function () {
     console.log("Listening to requrest on port 8080")
 });
 
-// Static files setup
-app.use(express.static('public'));
 
 // Socket setup (backend)
 var io = socket(server);
